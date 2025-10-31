7a3545bbbb405aeccef7d15e418749b9e6f4b68b
João Guilherme Silva Souza <56419147+smili1605@users.noreply.github.com>
2025-10-31 20:22:29 -0300

    Fix import syntax for PrismaService and UsersService

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {{PrismaService} from'/prisma.service';
-import {{UsersService} from'/user.service':
+import {PrismaService} from'/prisma.service';
+import {UsersService} from'/user.service':
 
 
 describe('AppController', () => {
